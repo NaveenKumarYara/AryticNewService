6ca21095da26a3d870abe64995eb7564058c6209
Yara Naveen Kumar <yaranaveenkumar@outlook.com>
2023-03-30 12:24:41 +0530

    Misc Changes

--- JobService/Program.cs
+++ JobService/Program.cs
@@ -2,6 +2,7 @@ using JobService.Context;
 using JobService.Contracts;
 using JobService.Repository;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
--- JobService/Repository/JobRepository.cs
+++ JobService/Repository/JobRepository.cs
@@ -3,29 +3,28 @@ using JobService.Context;
 using JobService.Contracts;
 using JobService.Entities;
 using MailChimp.Net.Models;
-
+using ConfigurationManager = System.Configuration.ConfigurationManager;
 namespace JobService.Repository
 {
     public class JobRepository : IJobRepository
     {
         private readonly DapperContext _context;
+        private  string? customerjobquery;
         public JobRepository(DapperContext context)
         {
             _context = context;
         }
         public async Task<IEnumerable<JobInfo>> GetCustomerJobs(long? CustomerId, long? UserId, int currentPageNumber, int pageSize)
         {
-            int maxPagSize = 50;
+            int maxPagSize = 100;
             pageSize = (pageSize > 0 && pageSize <= maxPagSize) ? pageSize : maxPagSize;
 
             int skip = (currentPageNumber - 1) * pageSize;
             int take = pageSize;
-
-            var query = "SELECT * FROM JobInfo WHERE CustomerId =  @CustomerId AND UserId=@UserId ORDER BY 1 DESC OFFSET @Skip ROWS FETCH NEXT @Take ROWS ONLY";
-
+            customerjobquery = ConfigurationManager.AppSettings["CustomerJobs"];
             using (var connection = _context.CreateConnection())
             {
-                var notes = await connection.QueryAsync<JobInfo>(query, new {CustomerId, UserId, Skip = skip, Take = take });
+                var notes = await connection.QueryAsync<JobInfo>(customerjobquery, new {CustomerId, UserId, Skip = skip, Take = take });
 
                 return notes.ToList();
             }
