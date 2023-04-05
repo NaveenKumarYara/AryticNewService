be41e5d250f8fe7226d1ba205d9421f80e3d6306
Yara Naveen Kumar <yaranaveenkumar@outlook.com>
2023-04-05 12:12:04 +0530

    Misc Changes

--- JobService/Repository/JobRepository.cs
+++ JobService/Repository/JobRepository.cs
@@ -16,12 +16,13 @@ namespace JobService.Repository
         }
         public async Task<IEnumerable<JobInfo>> GetCustomerJobs(long? CustomerId, long? UserId, int currentPageNumber, int pageSize)
         {
-            int maxPagSize = 100;
+            int maxPagSize = 50;
             pageSize = (pageSize > 0 && pageSize <= maxPagSize) ? pageSize : maxPagSize;
 
             int skip = (currentPageNumber - 1) * pageSize;
             int take = pageSize;
-            customerjobquery = ConfigurationManager.AppSettings["CustomerJobs"];
+            //customerjobquery = ConfigurationManager.AppSettings["CustomerJobs"];
+            customerjobquery = "SELECT * FROM JobInfo WHERE CustomerId =  @CustomerId AND UserId=@UserId ORDER BY 1 DESC OFFSET @Skip ROWS FETCH NEXT @Take ROWS ONLY";
             using (var connection = _context.CreateConnection())
             {
                 var notes = await connection.QueryAsync<JobInfo>(customerjobquery, new {CustomerId, UserId, Skip = skip, Take = take });
